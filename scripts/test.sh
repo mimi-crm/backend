8c9736a68d8ed5200555d75a88088bf561ac997b
mac <mac@macs-MacBook-Pro.local>
2024-12-02 09:41:24 +0900

    tput 삭제

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-COLOR_GREEN=`tput setaf 2;`
-COLOR_NC=`tput sgr0;` # No Color
-
 echo "Starting black"
 poetry run black ../src
 echo "OK"
@@ -12,10 +9,10 @@ echo "Starting isort"
 poetry run isort ../src
 echo "OK"
 
-#echo "Starting mypy"
-#poetry run mypy ../src
-#echo "OK"
+echo "Starting mypy"
+poetry run mypy ../src
+echo "OK"
 
 echo "Starting test with coverage"
 
-echo "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
+echo "All tests passed successfully!"
