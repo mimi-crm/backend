efcd52a6d746888f3caa50bf66730e0989d18c77
mac <mac@macs-MacBook-Pro.local>
2024-12-02 09:44:08 +0900

    test.sh 경로 수정

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,15 +2,15 @@
 set -eo pipefail
 
 echo "Starting black"
-poetry run black ../src
+poetry run black ./src
 echo "OK"
 
 echo "Starting isort"
-poetry run isort ../src
+poetry run isort ./src
 echo "OK"
 
 echo "Starting mypy"
-poetry run mypy ../src
+poetry run mypy ./src
 echo "OK"
 
 echo "Starting test with coverage"
